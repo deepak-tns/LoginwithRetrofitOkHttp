948549cfcb8caa873b0f6397320d1dde48732cc0
deepak-tns <deepaksachan016@gmail.com>
2019-05-22 16:28:05 +0530

    commit 3

--- app/src/main/java/com/india/lhq/loginwithretrofitokhttp/MainActivity.java
+++ app/src/main/java/com/india/lhq/loginwithretrofitokhttp/MainActivity.java
@@ -2,14 +2,22 @@ package com.india.lhq.loginwithretrofitokhttp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         showCart();
+    }
+
+    @Override
+    public int getLayoutId() {
+        return R.layout.activity_main;
     }
 
     void showCart() {
